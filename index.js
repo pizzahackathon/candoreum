3681669d5566496d4d168cbfa9eeb06805fba061
Nattapon Nimakul <totizy@gmail.com>
2018-08-18 10:02:33 +0700

    update receiver address

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import Web3 from 'web3'
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 const ISSUE_CANDY_COMMAND = '/home/pi/run.sh'
+const RECEIVER_ADDRESS = '0x7357c4eb39e8e7c4d66635e2d76b343be759c88b'
 
 
 // Issue candy command
@@ -23,7 +24,7 @@ var subscription = web3.eth.subscribe('pendingTransactions')
     }
 
     // Watch on incoming ether to 0x950807aeaccb5e66dc09e9f99a7d559a880d8b14
-		if (result.to.toLocaleLowerCase() === '0x950807aeaccb5e66dc09e9f99a7d559a880d8b14'.toLocaleLowerCase()) {
+		if (result.to.toLocaleLowerCase() === RECEIVER_ADDRESS.toLocaleLowerCase()) {
       console.log('from: ' + result.from + ' to: ' + result.to + ' tx: ' + transaction)
       issueCandy()
     }
