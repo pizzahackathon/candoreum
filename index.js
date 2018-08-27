57ab3a54cf7b1c6d925d727000dcdc59e4da65ab
Nattapon Nimakul <totizy@gmail.com>
2018-08-27 19:36:12 +0700

    add more comments to code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ISSUE_CANDY_COMMAND = '/home/pi/run.sh'
 const RECEIVER_ADDRESS = '0x7357c4eb39e8e7c4d66635e2d76b343be759c88b'
 
 
-// Issue candy command
+// Issue candy command, use IR transmit to send signal to candy machine.
 async function issueCandy() {
   const { stdout, stderr } = await exec(ISSUE_CANDY_COMMAND)
   console.log('stdout:', stdout)
@@ -22,18 +22,22 @@ const web3 = new Web3('wss://pzcethnode.afourleaf.com:28546')
 // Subsribe to pending transaciton
 var subscription = web3.eth.subscribe('pendingTransactions')
 .on("data", transaction => {
-	web3.eth.getTransaction(transaction).then(result => { 
+  web3.eth.getTransaction(transaction).then(result => {
+    // Found a new transaciton
+    // Validate transaction info
     if (typeof result === 'undefined' || result == null || typeof result.to === 'undefined' || result.to === null) {
       return
     }
 
+    // For debug uncomment this line
     // console.log('from: ' + result.from + ' to: ' + result.to + ' tx: ' + transaction)
-    // Watch on incoming ether to 0x950807aeaccb5e66dc09e9f99a7d559a880d8b14
-		if (result.to.toLocaleLowerCase() === RECEIVER_ADDRESS.toLocaleLowerCase()) {
+
+    // Watch on incoming ether to RECEIVER_ADDRESS
+    if (result.to.toLocaleLowerCase() === RECEIVER_ADDRESS.toLocaleLowerCase()) {
       console.log('from: ' + result.from + ' to: ' + result.to + ' tx: ' + transaction)
       issueCandy()
     }
   })
 })
 
-console.log('hello world')
+console.log(`Ready, feed me some ETHs to ${RECEIVER_ADDRESS}`)
